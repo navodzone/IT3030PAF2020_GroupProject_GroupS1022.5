d4074840788f81567ca0906a0f3d036fa555374a
PMGurusinghe <madhushangurusinghe@gmail.com>
2020-04-17 15:40:46 +0530

    impliment DBConnection And Insert part

--- PMGurusinghe/Patient_REST/src/com/PatientService.java
+++ PMGurusinghe/Patient_REST/src/com/PatientService.java
@@ -2,6 +2,50 @@ package com;
 
 import model.Patient;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-	
+//For REST Service import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Patient")
+public class PatientService {
+	Patient pObj = new Patient();
+
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertPatient(
+			@FormParam("FirstName") String FName, 
+			@FormParam("LastName") String LName,
+			@FormParam("NIC") String NIC,
+			@FormParam("DOB") String DOB,
+			@FormParam("Email") String Email,
+			@FormParam("Mobile") String Mobile,
+			@FormParam("Address") String Address,
+			@FormParam("BloodGroup") String BloodGroup,
+			@FormParam("Allergy") String Allergy,
+			@FormParam("Gender") String Gender,
+			@FormParam("password") String password,
+			@FormParam("ConfirmPassword") String ConfirmPassword) 
+	{
+		String output = pObj.insertPatient(FName, LName, NIC, DOB, Email, Mobile, Address,BloodGroup, Allergy, Gender, password, ConfirmPassword);
+		return output;
+	}
 }
--- PMGurusinghe/Patient_REST/src/model/Patient.java
+++ PMGurusinghe/Patient_REST/src/model/Patient.java
@@ -19,4 +19,60 @@ public Connection connect() {
 		return con;
 	}
 
+public String insertPatient(String FName, String LName, String NIC,String DOB,String Email,String Mobile,String Address,String BloodGroup, String Allergy,String Gender,String password,String ConfirmPassword)
+		
+		 
+{
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+
+			if (con == null) 
+			{
+				return "Error while connecting to the database for inserting.";
+			}
+
+			// create a prepared statement
+			String query = " insert into patient(`Patient_ID`,`FirstName`,`LastName`,`NIC`,`DOB`,`Email`,`Mobile`,`Address`,`BloodGroup`,`Allergy`,`Gender`,`password`,`ConfirmPassword`)" + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; 
+			 
+			 
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, FName);
+			preparedStmt.setString(3, LName);
+			preparedStmt.setString(4, NIC);
+			preparedStmt.setString(5, DOB);
+			preparedStmt.setString(6, Email);
+			preparedStmt.setString(7, Mobile);
+			preparedStmt.setString(8, Address);
+			preparedStmt.setString(9, BloodGroup);
+			preparedStmt.setString(10, Allergy);
+			preparedStmt.setString(11, Gender);
+			preparedStmt.setString(12, password);
+			preparedStmt.setString(13, ConfirmPassword);
+		
+			/* preparedStmt.setDouble(4, Double.parseDouble(price)); */
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Inserted successfully";
+			
+		   }
+		
+		catch (Exception e) 
+		{
+			output = "Error while inserting the Patient Details.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+
+	}
+
 }
