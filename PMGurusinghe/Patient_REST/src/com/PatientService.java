2f82f6924aeea8d67adb703fd5a552e75c598609
PMGurusinghe <madhushangurusinghe@gmail.com>
2020-04-17 15:06:55 +0530

    minor changes in Patient_REST

---
+++ PMGurusinghe/Patient_REST/src/com/PatientService.java
@@ -0,0 +1,7 @@
+package com;
+
+import model.Patient;
+
+
+	
+}
---
+++ PMGurusinghe/Patient_REST/src/model/Patient.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.sql.*;
+
+public class Patient {
+
+public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/patient", "root", "");
+
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
